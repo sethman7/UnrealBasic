fe9ef195e6bb882ba692a45400cf1be021972d51
sethman7 <131145377+sethman7@users.noreply.github.com>
2025-02-26 15:11:56 +0900

    Add 'PackageManagement/' from commit 'b324b9b47551fc0b1622202d31ba9bfdf83f07ef'
    git-subtree-dir: PackageManagement
    git-subtree-mainline: b324b9b47551fc0b1622202d31ba9bfdf83f07ef
    git-subtree-split: b324b9b47551fc0b1622202d31ba9bfdf83f07ef

---
+++ PackageManagement/MemoryManagement/Source/UnrealMemory/UnrealMemoryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealMemoryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALMEMORY_API AUnrealMemoryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
